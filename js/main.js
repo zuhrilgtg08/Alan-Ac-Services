110a89910c1e45bf1e17d6f0d79b799905bfc86f
zuhrilgtg08 <zuhrilfahrizal08gtg@gmail.com>
2023-11-01 14:56:26 +0700

    init-commit-1-11-2023

---
+++ js/main.js
@@ -0,0 +1,44 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });    
+})(jQuery);
+
+// Active Menu Navbar
+let aListMenu = document.querySelectorAll(".list-menu");
+
+aListMenu.forEach((a) => {
+    a.addEventListener('click', function (e) {
+        aListMenu.forEach((el) => el.classList.remove("active"));
+        a.classList.add("active");
+    })
+})
